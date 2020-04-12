1882e1e38b589e6c8725802a1452331b90c3c3f4
Mahmud <ajaomahmud@gmail.com>
2020-04-12 13:34:19 +0100

    feat: created the product model

---
+++ src/database/models/product.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    'Product',
+    {
+      product_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      service_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      service_charge: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      image: DataTypes.STRING(255),
+      description: DataTypes.STRING(255),
+    },
+    {
+      tableName: 'products',
+    }
+  );
+  Product.associate = ({ Service }) => {
+    // associations can be defined here
+    Product.belongsTo(Service, {
+      foreignKey: 'service_id',
+    });
+  };
+  return Product;
+};
