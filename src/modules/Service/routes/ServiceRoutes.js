9790a23e399a486171eb3ce24910eda1eb0f9f5d
Mahmud <ajaomahmud@gmail.com>
2020-04-12 13:21:09 +0100

    feat: created the service routes file

---
+++ src/modules/Service/routes/ServiceRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ServiceController from '../controller/ServiceController';
+import verify from '../../../middleware/verify';
+
+const router = Router();
+router.post('/create', verify, ServiceController.createNewService);
+router.put('/:id', verify, ServiceController.updateService);
+router.get('/', verify, ServiceController.getAllServices);
+router.get('/search', verify, ServiceController.searchServices);
+router.delete('/', verify, ServiceController.deleteService);
+
+export default router;
