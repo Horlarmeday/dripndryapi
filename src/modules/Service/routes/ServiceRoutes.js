faca5291a33f05973db9604d67e3c97feddef66e
Mahmud <ajaomahmud@gmail.com>
2020-04-13 17:21:55 +0100

    chore: added the get all products in a service route

--- src/modules/Service/routes/ServiceRoutes.js
+++ src/modules/Service/routes/ServiceRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.post('/create', verify, ServiceController.createNewService);
 router.put('/:id', verify, ServiceController.updateService);
 router.get('/', verify, ServiceController.getAllServices);
+router.get('/:serviceId', verify, ServiceController.getAllProductsInAService);
 router.get('/search', verify, ServiceController.searchServices);
 router.delete('/', verify, ServiceController.deleteService);
 
