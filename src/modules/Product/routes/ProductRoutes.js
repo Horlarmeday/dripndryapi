f24354b1c0f191ff15ced06f63738cde475826b4
Mahmud <ajaomahmud@gmail.com>
2020-04-25 21:12:23 +0100

    chore: added authorization to some routes

--- src/modules/Product/routes/ProductRoutes.js
+++ src/modules/Product/routes/ProductRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import ProductController from '../controller/ProductController';
 import verify from '../../../middleware/verify';
+import { authorize } from '../../../middleware/authorize';
+import Role from '../../../helpers/roles';
 
 const router = Router();
-router.post('/:serviceId/create', verify, ProductController.createNewProduct);
-router.put('/:id', verify, ProductController.updateProduct);
-router.get('/', ProductController.getAllProducts);
-router.delete('/', ProductController.deleteProduct);
+router.post(
+  '/:serviceId/create',
+  verify,
+  authorize(Role.SuperAdmin, Role.Admin),
+  ProductController.createNewProduct
+);
+router.put('/:id', verify, authorize(Role.SuperAdmin, Role.Admin), ProductController.updateProduct);
+router.get('/', authorize(Role.SuperAdmin, Role.Admin), ProductController.getAllProducts);
+router.delete('/', authorize(Role.SuperAdmin, Role.Admin), ProductController.deleteProduct);
 
 export default router;
