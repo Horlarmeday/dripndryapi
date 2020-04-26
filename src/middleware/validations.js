3e3c10e63c465b1d1ce15fb9d1bc89a3b31d52c2
Mahmud <ajaomahmud@gmail.com>
2020-04-26 12:22:52 +0100

    feat: created a new function to validate admin login and orders

--- src/middleware/validations.js
+++ src/middleware/validations.js
@@ -3,11 +3,11 @@ import Joi from '@hapi/joi';
 export function validateCustomer(customer) {
   const schema = Joi.object({
     firstname: Joi.string()
-      .min(4)
+      .min(3)
       .max(50)
       .required(),
     lastname: Joi.string()
-      .min(4)
+      .min(3)
       .max(50)
       .required(),
     email: Joi.string()
@@ -68,4 +68,63 @@ export function validateNewProduct(product) {
       .allow(''),
   });
   return schema.validate(product);
+}
+
+export function validateNewOrder(order) {
+  const schema = Joi.object({
+    total_amount: Joi.number().required(),
+    comments: Joi.string()
+      .max(255)
+      .optional()
+      .allow(''),
+    data: Joi.array()
+      .items(Joi.object())
+      .required(),
+  });
+  return schema.validate(order);
+}
+
+export function validateAdminAccount(admin) {
+  const schema = Joi.object({
+    firstname: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    lastname: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    phone: Joi.string()
+      .min(11)
+      .required(),
+    address: Joi.string()
+      .max(100)
+      .required(),
+    role: Joi.string().required(),
+  });
+  return schema.validate(admin);
+}
+
+export function validateAdminLogin(admin) {
+  const schema = Joi.object({
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    email: Joi.string()
+      .email()
+      .min(5)
+      .max(255)
+      .required(),
+  });
+  return schema.validate(admin);
 }
